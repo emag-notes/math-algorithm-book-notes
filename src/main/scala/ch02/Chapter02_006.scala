0e843e1e5c5e32705e37035207b6d45f20a68c02
Yoshimasa Tanabe <yoshimasa_tanabe@funds.jp>
2022-01-05 19:39:50 +0900

    Done chapter 2.4.3

---
+++ src/main/scala/ch02/Chapter02_006.scala
@@ -0,0 +1,11 @@
+package ch02
+
+import java.util.Scanner
+
+object Chapter02_006 {
+  def main(args: Array[String]): Unit = {
+    val sc = new Scanner(System.in)
+    val N = sc.nextInt()
+    println(2 * N + 3)
+  }
+}
